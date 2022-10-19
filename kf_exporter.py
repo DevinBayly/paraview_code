ea617504daaa423956d3aae1892d221133e6934c
Devin Bayly <baylyd@email.arizona.edu>
2022-10-19 15:08:38 -0700

    Update and rename kf_exporter to kf_exporter.py

--- kf_exporter.py
+++ kf_exporter.py
@@ -8,8 +8,11 @@ cue = GetCameraTrack(view = rv)
 kfs = cue.KeyFrames
 pth = Path(Path.home()/".config/Paraview/Animation_Cues/")
 pth.mkdir(exist_ok=True,parents=True)
-
-with open(f"{pth}/paraview_animation_{uuid.uuid1()}","w") as phile:
+# finding suitable name
+src = GetActiveSource()
+pxm = servermanager.ProxyManager()
+name = pxm.GetProxyName("sources",src)
+with open(f"{pth}/paraview_animation_{name}_{uuid.uuid1()}","w") as phile:
     all_kfs = []
     for kf in kfs:
         all_kfs.append([kf.KeyTime ,list(kf.Position) ,list(kf.FocalPoint) ,list(kf.ViewUp) ])
