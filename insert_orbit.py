1740eaf1f807a158c90d436c02b686ee999641ea
Devin Bayly <baylyd@email.arizona.edu>
2023-02-13 18:22:22 -0700

    made changes on HPC

--- insert_orbit.py
+++ insert_orbit.py
@@ -1,23 +1,36 @@
 from paraview.simple import *
 
 anim = GetAnimationScene()
+
 renderView1 = GetActiveViewOrCreate("RenderView")
 cameraAnimationCue1 = CameraAnimationCue()
 #cameraAnimationCue1 = GetCameraTrack(view=rv)
 cameraAnimationCue1.Mode = 'Path-based'
 cameraAnimationCue1.AnimatedProxy = renderView1
 # create a new key frame
-n = 3 
-for i in range(n):
+s = GetActiveSource()
+d = s.GetDataInformation()
+n = d.DataInformation.GetNumberOfTimeSteps()
+# if we decide that we want a certain number of frames per revolution we should multiple the number of plants by that amount and set that as the animations number of frames
+# logic is that if we have a person watching for about a minute (600 frames at 10fps) and we have n orbits to complete then we will need to move at a different number of frames
+total_seconds = 30
+target_fps = 10
+frames_per_orbit  = ((total_seconds*target_fps)//n)+1
+anim.NumberOfFrames = (total_seconds*target_fps)
+## if we do more orbits then I think people will get dizzy
+orbits = 3
+for i in range(orbits):
     keyFrameN = CameraKeyFrame()
     keyFrameN.Position = [-6.6921304299024635, 0.0, 0.0]
     keyFrameN.FocalPoint = [1e-20, 0.0, 0.0]
     keyFrameN.ViewUp = [0.0, 0.0, 1.0]
     keyFrameN.ParallelScale = 1.7320508075688772
-    keyFrameN.PositionPathPoints = [0.0, -5.0, 0.0, 2.938926261462365, -4.045084971874736, 0.0, 4.755282581475766, -1.545084971874737, 0.0, 4.755282581475766, 1.5450849718747361, 0.0, 2.938926261462365, 4.045084971874735, 0.0, 1.3322676295501878e-15, 4.9999999999999964, 0.0, -2.9389262614623624, 4.045084971874735, 0.0, -4.755282581475763, 1.5450849718747368, 0.0, -4.755282581475763, -1.5450849718747341, 0.0, -2.9389262614623632, -4.045084971874731, 0.0]
+    keyFrameN.PositionPathPoints = [-0.027969280712061628, -0.5221370489856471, 0.5202375330914683, 0.27053340707321527, -0.42514764634574914, 0.5202375330914683, 0.4550182138577148, -0.17122609368594643, 0.5202375330914683, 0.4550182138577148, 0.14263820635386187, 0.5202375330914683, 0.27053340707321527, 0.39655975901366425, 0.5202375330914683, -0.027969280712061496, 0.4935491616535621, 0.5202375330914683, -0.32647196849733834, 0.39655975901366425, 0.5202375330914683, -0.5109567752818371, 0.142638206353862, 0.5202375330914683, -0.5109567752818371, -0.17122609368594618, 0.5202375330914683, -0.3264719684973385, -0.4251476463457486, 0.5202375330914683]
     keyFrameN.FocalPathPoints = [0.0, 0.0, 0.0]
     keyFrameN.ClosedPositionPath = 1 
-    keyFrameN.KeyTime = i/n 
+# can't use normalized time here because the ply reader sets the model changes in index time
+    keyFrameN.KeyTime = i/orbits 
+    print(i/orbits,"time")
     cameraAnimationCue1.KeyFrames.append(keyFrameN)
 
 
