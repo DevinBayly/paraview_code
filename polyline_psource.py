e0fb5570409d358c3313f8b33f1a0c58257bc2ac
devinbayly <baylyd@arizona.edu>
2023-08-11 12:03:48 -0700

    experiment loading numpy rands as polyline

--- polyline_psource.py
+++ polyline_psource.py
@@ -1,6 +1,7 @@
 ## programmable source code for a polyline, based on https://examples.vtk.org/site/Python/GeometricObjects/PolyLine/
 from vtkmodules.vtkCommonDataModel import vtkCellArray,vtkPolyData,vtkPolyLine
 from vtkmodules.vtkCommonCore import vtkPoints
+import numpy as np
 print(vtkCellArray)
 
 origin = [0.0, 0.0, 0.0]
@@ -10,14 +11,15 @@ p2 = [0.0, 1.0, 2.0]
 p3 = [1.0, 2.0, 3.0]
 
 points = vtkPoints()
-points.InsertNextPoint(origin)
-points.InsertNextPoint(p0)
-points.InsertNextPoint(p1)
-points.InsertNextPoint(p2)
-points.InsertNextPoint(p3)
+rands = np.random.random((200,3))
+for row in rands[:]:
+  points.InsertNextPoint(list(row))
+
+print(points)
+
 polyline = vtkPolyLine()
-polyline.GetPointIds().SetNumberOfIds(5)
-for i in range(5):
+polyline.GetPointIds().SetNumberOfIds(rands.shape[0])
+for i in range(rands.shape[0]):
   polyline.GetPointIds().SetId(i,i)
 
 print(polyline)
